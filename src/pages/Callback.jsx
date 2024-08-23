cf8d2e45a3756dc063763b0ae3e4c7ed1102bd40
suraj sakhare <sakharesuraj10@gmail.com>
2024-08-23 15:39:54 +0530

    env updated

--- src/pages/Callback.jsx
+++ src/pages/Callback.jsx
@@ -1,6 +1,5 @@
-// Callback.js
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Cookies from 'js-cookie';
 
 const Callback = () => {
@@ -8,20 +7,25 @@ const Callback = () => {
 
   useEffect(() => {
     const hash = window.location.hash;
-    let token = null;
-    
     if (hash) {
-      const urlParams = new URLSearchParams(hash.substring(1));
-      token = urlParams.get("access_token");
-      
-      if (token) {
-        Cookies.set("spotifyAuthToken", token, { expires: 1 }); // Store token in cookies
-        navigate("/");
-      }
+      const token = hash
+        .substring(1)
+        .split("&")
+        .find(elem => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      // Store the token in cookies
+      Cookies.set("spotify_access_token", token, { expires: 1, sameSite: 'None', secure: true });
+
+      // Clear the hash from the URL
+      window.location.hash = "";
+
+      // Redirect to the main app
+      navigate("/");
     }
   }, [navigate]);
 
-  return <div>Loading...</div>;
+  return <div>Logging in...</div>;
 };
 
 export default Callback;
